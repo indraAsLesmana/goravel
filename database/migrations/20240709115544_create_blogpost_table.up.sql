6869d8cfdb3d7f4d437e15b7714774da0b2de144
Indra Agus Lesmana <indra.lesmana07@homecredit.co.id>
2024-07-09 22:55:26 +0000

    adjust table according to model

--- database/migrations/20240709115544_create_blogpost_table.up.sql
+++ database/migrations/20240709115544_create_blogpost_table.up.sql
@@ -2,6 +2,10 @@ CREATE TABLE blogpost (
   id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   created_at datetime(3) NOT NULL,
   updated_at datetime(3) NOT NULL,
+  title varchar(255) NOT NULL,
+  attach_file text,
+  url varchar(255),
+  deleted_at datetime(3),
   PRIMARY KEY (id),
   KEY idx_blogpost_created_at (created_at),
   KEY idx_blogpost_updated_at (updated_at)
