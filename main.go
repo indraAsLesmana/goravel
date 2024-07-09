c9b8f751f7c4f9b175d541f36121ee7a02e575b4
Indra Agus Lesmana <indra.lesmana07@homecredit.co.id>
2024-07-09 03:16:36 +0000

    add database connection check

--- main.go
+++ main.go
@@ -1,21 +1,59 @@
 package main
 
 import (
-	"github.com/goravel/framework/facades"
+	"fmt"
+	"log"
+	"os"
 
 	"goravel/bootstrap"
+
+	"github.com/goravel/framework/facades"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
 	// This bootstraps the framework and gets it ready for use.
 	bootstrap.Boot()
 
-	//Start http server by facades.Route().
+	// Start http server by facades.Route().
 	go func() {
 		if err := facades.Route().Run(); err != nil {
 			facades.Log().Errorf("Route run error: %v", err)
 		}
 	}()
+
+	// Debug: print environment variables
+	fmt.Println("DB_HOST:", os.Getenv("DB_HOST"))
+	fmt.Println("DB_PORT:", os.Getenv("DB_PORT"))
+	fmt.Println("DB_DATABASE:", os.Getenv("DB_DATABASE"))
+	fmt.Println("DB_USERNAME:", os.Getenv("DB_USERNAME"))
+	fmt.Println("DB_PASSWORD:", os.Getenv("DB_PASSWORD"))
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_DATABASE"))
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("failed to get database instance: %v", err)
+	}
+
+	// Ping the database
+	err = sqlDB.Ping()
+	if err != nil {
+		log.Fatalf("failed to ping database: %v", err)
+	}
+
+	log.Println("successfully connected to the database")
 
 	select {}
 }
